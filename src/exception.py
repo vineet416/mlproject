006e6b05d4ccf672cd1094b892f317ef4ad2dd5d
vineet416 <vineetpatel468@gmail.com>
2025-04-01 17:41:38 +0530

    Logging and Exception

---
+++ src/exception.py
@@ -0,0 +1,38 @@
+import sys
+import logging
+
+def error_message_detail(error, error_detail: sys):
+    """
+    Extracts the error message from the exception and returns it as a string.
+    
+    Args:
+        error (Exception): The exception object.
+        error_detail (sys): The sys module to extract the traceback information.
+    
+    Returns:
+        str: The detailed error message.
+    """
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error)
+    )
+    return error_message
+    
+
+
+
+class CustomException(Exception):
+    """
+    Custom exception class that inherits from the built-in Exception class.
+    
+    Args:
+        Exception (Exception): The base exception class.
+    """
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+    
